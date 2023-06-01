cbd1f78d59fbe7119657e71d1e7854c88710a090
Juan Pablo Gonzalez <juanpa.g.927@gmail.com>
2023-06-01 16:05:23 -0500

    end-points metrics

--- app.py
+++ app.py
@@ -8,6 +8,83 @@ from datetime import datetime
 app = Flask(__name__)
 api = Api(app)
 
+base_query = '''WITH base_query AS (
+	SELECT	
+		emp.id AS empId, 
+		emp.ename AS name, 
+		emp.datetime AS datetime, 
+		jobs.id AS jobId, 
+		jobs.job AS job, 
+		dep.id AS depId, 
+		dep.department AS department 
+	FROM dbo.hired_employees emp
+	inner join dbo.jobs jobs
+		ON emp.job_id = jobs.id
+	inner join dbo.departments dep
+		ON emp.department_id = dep.id
+	WHERE year(emp.datetime) = 2021
+),
+
+ALLQ AS(
+	SELECT	
+		job, 
+		department,	
+		DATEPART(QUARTER, datetime) as Q, 
+		COUNT(*) AS amount 
+	FROM base_query
+	GROUP BY job, department, DATEPART(QUARTER, datetime)
+),
+'''
+
+quarters_query = base_query + \
+'''
+quarters AS(
+	SELECT 
+		department, 
+		job, 
+		COALESCE([1], 0) Q1,
+		COALESCE([2], 0) Q2,
+		COALESCE([3], 0) Q3,
+		COALESCE([4], 0) Q4 
+	FROM ALLQ
+	PIVOT(MAX(amount)
+	FOR Q IN ([1],[2],[3],[4])) q
+)
+
+SELECT * FROM quarters
+ORDER BY department, job;'''
+
+median_query = base_query + \
+'''MEDIAN AS(
+	SELECT AVG(TOTAL) MEDIA 
+	FROM(
+		SELECT SUM(AMOUNT) TOTAL from ALLQ
+		GROUP BY department
+	) FINAL
+),
+
+TOTAL_DEP AS(
+	select 
+		depId AS id, 
+		department, 
+		COUNT(*) hired from 
+	base_query
+	GROUP BY depId, department
+),
+
+MAY_DEP AS(
+	SELECT TOT_DEP.* FROM (
+		SELECT * FROM TOTAL_DEP
+	)TOT_DEP,
+	(
+		SELECT * FROM MEDIAN
+	)MED
+	WHERE TOT_DEP.hired > MED.MEDIA
+)
+
+SELECT * FROM MAY_DEP
+ORDER BY hired DESC;'''
+
 server = 'sqlserverchallenge.database.windows.net'
 database = 'sqldb_challenge'
 username = 'sql_admin'
@@ -24,8 +101,29 @@ filesSchema = {'hired_employees':{'id':'int64',
                 'jobs':{'id':'int64',
                         'job':'object'}}
 
+@app.route('/totalDep')
+def totalDep():
+    cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+    cursor = cnxn.cursor()
 
+    df = pd.read_sql(quarters_query, cnxn)
 
+    cnxn.commit()
+    cursor.close()
+
+    return render_template('index.html',  tables=[df.to_html(classes='data')], titles=df.columns.values)
+
+@app.route('/mostDep')
+def mostDep():
+    cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+    cursor = cnxn.cursor()
+
+    df = pd.read_sql(median_query, cnxn)
+
+    cnxn.commit()
+    cursor.close()
+
+    return render_template('index.html',  tables=[df.to_html(classes='data')], titles=df.columns.values)
 
 
 class csv_migration(Resource):
